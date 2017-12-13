45f41e70e857aac920e198d88ae50a7b8eca73d4
沈陈 <shenchen@cnepay.com>
2017-12-13 10:28:22 +0800

    1.添加项目boundle ID;2.添加ClassPreFIx

---
+++ AggregatePay/Business/Auth/SettlementCardAuth/Controller/APSettlementCardAuthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  APSettlementCardAuthViewController.swift
+//  AggregatePay
+//
+//  Created by 沈陈 on 2017/12/13.
+//  Copyright © 2017年 bingtianyu. All rights reserved.
+//
+
+import UIKit
+
+class APSettlementCardAuthViewController: APAuthBaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
