c3d3c899fb184f58d96a232024aba3baced767ca
邴天宇 <bingty@cnepay.com>
2017-12-14 22:13:56 +0800

    提交代码:我的页面UI完成

--- AggregatePay/Business/Mine/View/APBankCardManageListView.swift
+++ AggregatePay/Business/Mine/View/APBankCardManageListView.swift
@@ -11,8 +11,11 @@ import UIKit
 let K_Width = UIScreen.main.bounds.size.width
 let K_Height = UIScreen.main.bounds.size.height
 
-class APBankCardManageListView: UIView, UITableViewDataSource, UITableViewDelegate, BWSwipeRevealCellDelegate {
-
+class APBankCardManageListView: UIView, UITableViewDataSource, UITableViewDelegate, APBankCardCellDelegate {
+    
+    var rows : Int = 10
+    let cellHeight : CGFloat = 110
+    
     lazy var tableView: UITableView = {
         var view = UITableView(frame: CGRect.zero, style: UITableViewStyle.plain)
         view.delegate = self;
@@ -25,7 +28,7 @@ class APBankCardManageListView: UIView, UITableViewDataSource, UITableViewDelega
     }()
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return rows
     }
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -33,25 +36,35 @@ class APBankCardManageListView: UIView, UITableViewDataSource, UITableViewDelega
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+        return cellHeight
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = APBankCardCell.cellWithTableView(tableView)
+        let cell : APBankCardCell = APBankCardCell.cellWithTableView(tableView) as! APBankCardCell
+        cell.delegate = self
 //        let swipeCell : BWSwipeRevealCell = cell as! BWSwipeRevealCell
 //        swipeCell.delegate = self as BWSwipeRevealCellDelegate
 //        swipeCell.threshold = 70
-        return cell!
+        return cell
         
     }
     
-    func swipeCellActivatedAction(_ cell: BWSwipeCell, isActionLeft: Bool) {
-        print("Swipe Cell Activated Action")
-        let indexPath: IndexPath = tableView.indexPath(for: cell)!
+    func swipeCellDelButtonAction(_ cell: UITableViewCell) {
+        let indexPath = tableView.indexPath(for: cell)
         
-//        self.removeObjectAtIndexPath(indexPath)
+        rows -= 1
+        tableView.deleteRows(at: [indexPath!], with: UITableViewRowAnimation.right)
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: false)
+        let cells = tableView.visibleCells
+        for cell in cells
+        {
+            (cell as! APBankCardCell).resetFrame()
+        }
+        
+    }
 //    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
 //        return true
 //    }
@@ -67,7 +80,17 @@ class APBankCardManageListView: UIView, UITableViewDataSource, UITableViewDelega
         
         let segmentView = APBankCardManageSegmentButton()
         segmentView.segmentBlock =  { index in
-            print(index)
+            if index == 0 {
+                self.rows = 5
+                self.tableView.reloadSections(IndexSet(integer: 0), with: UITableViewRowAnimation.left)
+            }
+            else
+            {
+                self.rows = 1
+                self.tableView.reloadSections(IndexSet(integer: 0), with: UITableViewRowAnimation.right)
+            }
+            
+           
         }
         
         addSubview(segmentView)
@@ -86,9 +109,6 @@ class APBankCardManageListView: UIView, UITableViewDataSource, UITableViewDelega
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-   
-
 }
 
 typealias AP_SegmentBlock = (_ index : Int)->Void
@@ -143,6 +163,9 @@ class APBankCardManageSegmentButton: UIView {
     
     @objc func leftButtonDidAction()
     {
+        if buttonLeft.isSelected == true {
+            return
+        }
         buttonRight.isSelected = false
         buttonLeft.isSelected = true
         if let block = segmentBlock {
@@ -163,6 +186,9 @@ class APBankCardManageSegmentButton: UIView {
     
     @objc func rightButtonDidAction()
     {
+        if buttonRight.isSelected == true {
+            return
+        }
         buttonRight.isSelected = true
         buttonLeft.isSelected = false
         if let block = segmentBlock {
@@ -187,9 +213,13 @@ class APBankCardManageSegmentButton: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
+protocol APBankCardCellDelegate : NSObjectProtocol{
+    func swipeCellDelButtonAction(_ cell : UITableViewCell)
+}
 //#makr - 
 class APBankCardCell: APSwipeTableViewCell {
+    
+    weak var delegate : APBankCardCellDelegate?
     
     static func cellWithTableView(_ tableView: UITableView) -> UITableViewCell? {
         let identifier = "APBankCardCell"
@@ -203,20 +233,16 @@ class APBankCardCell: APSwipeTableViewCell {
         return newCell
     }
     
+    var bankName : UILabel!
+    var userName : UILabel!
+    var bankNumber : UILabel!
+    var rightMsg : UILabel!
+
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         contentView.backgroundColor = UIColor.clear
         backgroundColor = UIColor.clear
-        
-//        self.revealDirection = .right
-//
-//        self.panElasticityFactor = 0.5
-//
-//        self.type = .slidingDoor
-//
-//        self.bgViewRightImage = UIImage(named:"BankCard_Del")!.withRenderingMode(.alwaysOriginal)
-//        self.bgViewRightColor = UIColor.black
         
         let bgImageView = UIImageView.init(image: UIImage.init(named: "BankCardBG"))
         
@@ -227,15 +253,36 @@ class APBankCardCell: APSwipeTableViewCell {
         
         swipeContentView.addSubview(bgImageView)
         contentView.addSubview(delButton)
-//        let bgView = UIView()
         
-//        bgView.backgroundColor = UIColor.clear
-//        bgView.addSubview(delButton)
-//        contentView.addSubview(bgView)
-//        contentView.addSubview(bgImageView)
-//        self.backView?.addSubview(delButton)
+        bankName = UILabel()
+        bankName.theme_textColor = ["#4c370b"]
+        bankName.font = UIFont.systemFont(ofSize: 14)
+        bankName.textAlignment = .left
         
+        userName = UILabel()
+        userName.theme_textColor = ["#4c370b"]
+        userName.font = UIFont.systemFont(ofSize: 12)
+        userName.textAlignment = .left
         
+        bankNumber = UILabel()
+        bankNumber.theme_textColor = ["#422f02"]
+        bankNumber.font = UIFont.systemFont(ofSize: 18)
+        bankNumber.textAlignment = .left
+        
+        rightMsg = UILabel()
+        rightMsg.theme_textColor = ["#4c370b"]
+        rightMsg.font = UIFont.systemFont(ofSize: 12)
+        rightMsg.textAlignment = .right
+        
+        bankName.text = "招商银行 信用卡"
+        userName.text = "张三"
+        bankNumber.text = "**** **** **** 3451"
+        rightMsg.text = "快捷"
+    
+        bgImageView.addSubview(bankName)
+        bgImageView.addSubview(userName)
+        bgImageView.addSubview(bankNumber)
+        bgImageView.addSubview(rightMsg)
         
         bgImageView.snp.makeConstraints { (make) in
             make.top.equalTo(10)
@@ -250,24 +297,35 @@ class APBankCardCell: APSwipeTableViewCell {
             make.width.height.equalTo(50)
         }
         
+        bankName.snp.makeConstraints { (make) in
+            make.top.equalTo(14)
+            make.left.equalTo(24)
+            make.height.equalTo(18)
+        }
+        userName.snp.makeConstraints { (make) in
+            make.top.equalTo(self.bankName.snp.bottom).offset(4)
+            make.left.equalTo(24)
+            make.height.equalTo(14)
+        }
+        bankNumber.snp.makeConstraints { (make) in
+            make.left.equalTo(24)
+            make.bottom.equalTo(-18)
+        }
+        rightMsg.snp.makeConstraints { (make) in
+            make.top.equalTo(15)
+            make.right.equalTo(-24)
+        }
     }
-//    override var frame:CGRect{
-//        didSet {
-//
-//            var newFrame = frame
-//            newFrame.origin.x += 20
-//            newFrame.size.width -= 40
-//            newFrame.origin.y += 10
-//            newFrame.size.height -= 10
-//            super.frame = newFrame
-//
-//        }
-//    }
   
     @objc func buttonDidAction()
     {
         print("删除")
         resetFrame()
+        guard let _delegate = delegate else {
+            return
+        }
+      
+        _delegate.swipeCellDelButtonAction(self)
     }
  
 
