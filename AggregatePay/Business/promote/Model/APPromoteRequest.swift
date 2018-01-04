cd6b92171189a0a3e2edebc5b9fb7e6166b83c28
邴天宇 <bingty@cnepay.com>
2018-01-04 18:13:17 +0800

    引导页完成

---
+++ AggregatePay/Business/promote/Model/APPromoteRequest.swift
@@ -0,0 +1,17 @@
+//
+//  APPromoteRequest.swift
+//  AggregatePay
+//
+//  Created by cnepayzx on 2018/1/4.
+//  Copyright © 2018年 bingtianyu. All rights reserved.
+//
+
+import UIKit
+
+class APPromoteRequest: APBaseRequest {
+    
+}
+
+class APPromoteResponse: APBaseResponse {
+    @objc dynamic var genCodeUrl : String?
+}
