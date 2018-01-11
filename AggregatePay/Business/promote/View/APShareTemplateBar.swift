69c340fb611a445a1b14141378ae060b85ff3e16
邴天宇 <bingty@cnepay.com>
2018-01-11 17:39:16 +0800

    启动页添加,推广模板扩充

--- AggregatePay/Business/promote/View/APShareTemplateBar.swift
+++ AggregatePay/Business/promote/View/APShareTemplateBar.swift
@@ -12,6 +12,10 @@ class APShareTemplateBar: UIView,UICollectionViewDelegate,UICollectionViewDataSo
 
     var delegate : AP_ActionProtocol?
     
+    var dataSource : [TemplateItemData] = [TemplateItemData.init("PromoteTemplate1", selected: true),
+                                           TemplateItemData.init("PromoteTemplate2", selected: false),
+                                           TemplateItemData.init("PromoteTemplate3", selected: false)]
+    
     
     lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
@@ -38,7 +42,7 @@ class APShareTemplateBar: UIView,UICollectionViewDelegate,UICollectionViewDataSo
     
     public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
     {
-        return 10
+        return dataSource.count
     }
     
     // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
@@ -46,12 +50,21 @@ class APShareTemplateBar: UIView,UICollectionViewDelegate,UICollectionViewDataSo
     public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell
     {
         let cell : APShareTemplateItemView = collectionView.dequeueReusableCell(withClass: APShareTemplateItemView.self, for: indexPath)!
+        let itemData = dataSource[indexPath.row];
+        cell.imageView.image = UIImage.init(named: itemData.imageName!)
+        cell.select = itemData.isSelected!
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let cell : APShareTemplateItemView = collectionView.cellForItem(at: indexPath) as! APShareTemplateItemView
         delegate?.AP_Action_Click?(cell.imageView.image as Any)
+        for item in dataSource {
+            item.isSelected = false
+        }
+        let itemData = dataSource[indexPath.row];
+        itemData.isSelected = true
+        collectionView.reloadData()
     }
     
     override init(frame: CGRect) {
@@ -73,8 +86,7 @@ class APShareTemplateItemView: UICollectionViewCell {
     let imageView : UIImageView = {
         let view = UIImageView()
         view.image = UIImage.init(named: "PromoteTemplate1")
-        view.borderColor = UIColor.green
-        view.borderWidth = 0.5
+      
         return view
     }()
     
@@ -92,6 +104,19 @@ class APShareTemplateItemView: UICollectionViewCell {
         return view
     }()
     
+    var select : Bool = true{
+        didSet{
+            if select {
+                imageView.borderColor = UIColor.green
+                imageView.borderWidth = 0.5
+            }
+            else
+            {
+                imageView.borderColor = UIColor.clear
+                imageView.borderWidth = 0.5
+            }
+        }
+    }
     
     
     override init(frame: CGRect) {
@@ -130,3 +155,13 @@ class APShareTemplateItemView: UICollectionViewCell {
         super.init(coder: aDecoder)
     }
 }
+
+class TemplateItemData {
+    var imageName : String?
+    var isSelected : Bool?
+    init(_ image:String,selected:Bool) {
+        imageName = image
+        isSelected = selected
+    }
+}
+
