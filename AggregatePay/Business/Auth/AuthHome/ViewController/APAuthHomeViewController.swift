6b4e1ffbaa40bc8c5ea149d44a590d8105ddfff1
邴天宇 <bingty@cnepay.com>
2018-01-16 19:16:51 +0800

    优化

--- AggregatePay/Business/Auth/AuthHome/ViewController/APAuthHomeViewController.swift
+++ AggregatePay/Business/Auth/AuthHome/ViewController/APAuthHomeViewController.swift
@@ -28,6 +28,11 @@ class APAuthHomeViewController: APBaseViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         loadAuthInfo()
     }
     
