b134dcd11eb25882c4581a6bad1d0c8ef60b67a0
xyd <xuyd@cnepay.com>
2018-01-18 17:55:56 +0800

    优化

--- AggregatePay/Business/promote/Controller/APPromoteViewController.swift
+++ AggregatePay/Business/promote/Controller/APPromoteViewController.swift
@@ -70,11 +70,11 @@ class APPromoteViewController: APBaseViewController,AP_ActionProtocol {
 
     @objc func shareAction(_ btn : UIButton)
     {
-        ap_userIdentityStatus {
+//        ap_userIdentityStatus {
             let shareC : APShareViewController = APShareViewController()
             shareC.shareImage = self.qrImageView.screenshot
             self.navigationController?.pushViewController(shareC)
-        }
+//        }
     }
     
     
