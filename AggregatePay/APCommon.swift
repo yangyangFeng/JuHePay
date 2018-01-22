c01ba45ffcb16dd2dc94259bed65a730ad829546
xyd <xuyd@cnepay.com>
2018-01-22 18:59:53 +0800

    主干生产环境

--- AggregatePay/APCommon.swift
+++ AggregatePay/APCommon.swift
@@ -28,5 +28,6 @@ let AP_WECHAT_KEY = "wx112291ad060761a0" //微信分享Key
     let AP_JPush_Key = "c28b33d238c98a7a0527e6a3"    //开发级推送证书（开发）
 #else
     //com.copay.aggregatepay.enterprise
-    let AP_JPush_Key = "50569eb637808b48e78f826b"    //企业级推送证书（生产）
+    let AP_JPush_Key = "c28b33d238c98a7a0527e6a3"    //企业级推送证书（生产）
+//    let AP_JPush_Key = "50569eb637808b48e78f826b"    //企业级推送证书（生产）
 #endif
